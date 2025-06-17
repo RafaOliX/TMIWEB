8137523e75bf68d789e09d6188c7a769c802b345
RafaOliX <rafaolix59@gmail.com>
2025-06-17 10:49:32 -0400

    prueba con mailersend

--- api/aplicar.js
+++ api/aplicar.js
@@ -33,12 +33,14 @@ export default async function handler(req, res) {
         });
       }
 
-      // Configura el transporte de correo para Gmail (o SMTP real)
+      // Configura el transporte de correo para Mailersend
       const transporter = nodemailer.createTransport({
-        service: 'gmail',
+        host: 'smtp.mailersend.net',
+        port: 587,
+        secure: false,
         auth: {
-          user: process.env.EMAIL_USER,
-          pass: process.env.EMAIL_PASS,
+          user: 'apikey', // Literalmente 'apikey'
+          pass: process.env.MAILERSEND_API_KEY,
         },
       });
 
